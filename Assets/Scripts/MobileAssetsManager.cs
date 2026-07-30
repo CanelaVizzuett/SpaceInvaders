0bb30b943061601875ed09c68741e13599e98173
cacaguaton <284833712+saidmtznav-commits@users.noreply.github.com>
2026-07-29 20:39:02 -0600

    Esteroide

--- Assets/Scripts/MobileAssetsManager.cs
+++ Assets/Scripts/MobileAssetsManager.cs
@@ -8,16 +8,24 @@ public class MobileAssetsManager : MonoBehaviour
     private GameObject[] desktopAssets;
     private void Awake()
     {
-        #if UNITY_IOS || UNITY_ANDROID
-        foreach (var asset in mobileAssets)
+        if (Application.isEditor)
         {
-            asset.SetActive(true);
+            SetActiveAssets(desktopAssets, true);
         }
-        #else
-        foreach (var asset in desktopAssets)
+        else if (Application.isMobilePlatform)
         {
-            asset.SetActive(true);
+            SetActiveAssets(mobileAssets, true);
         }
-        #endif
+        else
+        {
+            SetActiveAssets(desktopAssets, true);
+        }
+    }
+    private void SetActiveAssets(GameObject[] assets, bool isActive)
+    {
+        foreach (var asset in assets)
+        {
+            asset.SetActive(isActive);
+        }
     }
 }
---
+++ Assets/Scripts/PoolManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class PoolManager : MonoBehaviour
+{
+    public static PoolManager Instance {get; private set;}
+    private void Awake()
+    {
+        Instance = this;
+    }
+    private Dictionary<GameObject, Pool> pools = new Dictionary<GameObject, Pool>();
+    public void RegisterPrefab(GameObject prefab)
+    {
+        if (!pools.ContainsKey(prefab))
+        {
+            Pool newPool = new GameObject(prefab.name + " Pool").AddComponent<Pool>();
+            newPool.prefab = prefab;
+            newPool.transform.parent = transform;
+            pools.Add(prefab, newPool);
+        }
+    }
+    public GameObject GetObject(GameObject prefab, Vector3 position, bool isTurnedOff = false)
+    {
+        if (pools.TryGetValue(prefab, out Pool pool))
+        {
+            return pool.InstantiateObject(position, isTurnedOff);
+        }
+        else
+        {
+            RegisterPrefab(prefab);
+            return pools[prefab].InstantiateObject(position, isTurnedOff);
+        }
+    }
+}
